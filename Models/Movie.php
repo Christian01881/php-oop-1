123168560cb11b323f689537c9c5ae64b983f89c
Christian01881 <chrispamo01881@gmail.com>
2023-05-18 14:46:35 +0200

    Create class and object

---
+++ Models/Movie.php
@@ -0,0 +1,22 @@
+<?php 
+// Creo una classe
+class Movie
+{
+    public string $title;
+    public string $original_title;
+    public string $nationality;
+    public float $vote;
+    public string $image;
+    public $genre;
+
+    public function __construct($title, $original_title, $nationality, $vote, $image, $genre)
+    {
+        $this->title = $title;
+        $this->original_title = $original_title;
+        $this->nationality = $nationality;
+        $this->vote = $vote;
+        $this->image = $image;
+        $this->genre = $genre;
+    }
+
+}
